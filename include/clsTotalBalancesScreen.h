f31e4aafe746756cc8ebc0530186db2624d4490b
Mark Moreed <markmoreedf@gmail.com>
2026-01-31 23:25:16 +0200

    made show all balances screen functionality

---
+++ include/clsTotalBalancesScreen.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "clsScreen.h"
+#include "clsBankClient.h"
+#include "clsUtility.h"
+
+class clsTotalBalancesScreen :
+    protected clsScreen
+{
+public:
+    static void ShowTotalBalancesScreen()
+    {
+        system("cls");
+        float totalBalances = clsBankClient::GetTotalBalances();
+        vector <clsBankClient> clients = clsBankClient::GetClientsList();
+        vector<vector<string>> data;
+        clsScreen::_PrintHeader("Total Balances Screen. [" + to_string(clients.size()) + "] clients found");
+
+        vector<string> coloumsNames = { "Account No.", "Full Name", "Balance" };
+        const vector<short> widths = { 20, 30, 25 };
+
+        for (clsBankClient& cl : clients) {
+            data.push_back({ cl.AccountNumber, cl.FullName(), to_string(cl.Balance) });
+        }
+
+        clsUtility::PrintTable(coloumsNames, widths, data, "\n\n\t\t\t\tNo Clients Available In the System!\n\n", 18);
+        // hard coded for simplicity.. 
+        clsUtility::PrintTableFooter({ { "Total Balances" }, {  }, {  to_string(totalBalances)} }, widths, 79, 18);
+
+    }
+
+};
+
