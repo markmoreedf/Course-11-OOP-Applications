78ac2c0f9dfb3bfa4af9f5e48dcc8662b990339e
Mark Moreed <markmoreedf@gmail.com>
2026-02-01 23:53:47 +0200

    implemented all basic client features

--- src/Course 11___OOP Applications.cpp
+++ src/Course 11___OOP Applications.cpp
@@ -8,33 +8,6 @@ using namespace std;
 
 
 
-
-//static void DeleteClient()
-//{
-//    string AccountNumber = MyInputLibrary::ReadLine("Enter Account Number of the client to delete: ");
-//    clsBankClient client = clsBankClient::FindClient(AccountNumber);
-//    while(client.IsEmpty())
-//    {
-//        cout << "Client with Account Number " << AccountNumber << " not found.\n";
-//        AccountNumber = MyInputLibrary::ReadLine("Enter Account Number of the client to delete: ");
-//        client = clsBankClient::FindClient(AccountNumber);
-//    }
-//
-//    cout << "\nClient Data:\n";
-//    client.Print();
-//
-//    if (! MyInputLibrary::YesNo("Are you sure you want to delete this client? (Y/N): "))
-//        return;
-//
-//    if (client.Delete())
-//        cout << "Client deleted successfully.\n";
-//    else 
-//        cout << "Failed to delete client.\n";
-//    
-//}
-
-
-
 int main()
 {
     clsMainScreen::ShowMainMenue();
