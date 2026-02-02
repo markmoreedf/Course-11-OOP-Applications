075ebd3b5640f6802537188b93ba70035b55e35a
Mark Moreed <markmoreedf@gmail.com>
2026-02-02 19:01:26 +0200

    added ablility to shift the header to the right

--- include/clsScreen.h
+++ include/clsScreen.h
@@ -9,12 +9,16 @@ protected:
     {
         std::cout << spaces << message;
     }
-    static void _PrintHeader(const std::string & title)
+    static void _PrintHeader(const std::string & title, const short & shiftRight = 0)
     {
         std::cout << "\n\n";
-        std::cout << spaces;    clsUtility::PrintDeviderLine(title.length() + 12);
+        std::cout << spaces;    clsUtility::PrintDeviderLine(title.length() + 12, shiftRight);
+        for (short i = 0; i < shiftRight; i++)
+        {
+            std::cout << " ";
+        }
         std::cout << spaces     << "| === " + title + " === |\n";
-        std::cout << spaces;    clsUtility::PrintDeviderLine(title.length() + 12);
+        std::cout << spaces;    clsUtility::PrintDeviderLine(title.length() + 12, shiftRight);
         std::cout << "\n\n";
 
 
