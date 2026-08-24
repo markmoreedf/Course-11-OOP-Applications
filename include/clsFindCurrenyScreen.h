cc98889e1898bc06662dd85b9648eb6ebbb7ce84
Mark Moreed <markmoreedf@gmail.com>
2026-08-24 22:06:01 +0300

    added find currency by country, currency code or currency name

---
+++ include/clsFindCurrenyScreen.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "clsScreen.h"
+#include "clsCurrency.h"
+#include "MyInputLibrary.h"
+class clsFindCurrenyScreen : protected clsScreen
+{
+private:
+
+    static void _PrintCurrencyRecord(clsCurrency currency)
+    {
+        _Print("\n");
+        _Print("===================================\n");
+        _Print("Country Name  : " + currency.countryName + "\n");
+        _Print("Currency Code : " + currency.crCode + "\n");
+        _Print("Currency Name : " + currency.crName + "\n");
+        _Print("Rate (1$)     : " + to_string(currency.rate) + "\n");
+        _Print("===================================\n");
+    }
+
+    static void _PerformFindCurrency(string choice)
+    {
+        clsCurrency currency = clsCurrency::FindCurrencyByCountry(choice);
+        if (!currency.IsEmpty())
+        { return _PrintCurrencyRecord(currency); }
+
+        currency = clsCurrency::FindCurrencyByName(choice);
+        if (!currency.IsEmpty())
+        { return _PrintCurrencyRecord(currency); }
+
+        currency = clsCurrency::FindCurrencyByCode(choice);
+        if (!currency.IsEmpty())
+        { return _PrintCurrencyRecord(currency); }
+        
+        _Print("No currency found with the given input: " + choice + "\n");
+
+    }
+
+public:
+
+    static void ShowFindCurrencyScreen()
+    {
+        while(true)
+        {
+            system("cls");
+            _PrintHeader("Find Currency Screen");
+            _Print("___________________________________\n\n");
+
+            _Print("Search By Country Name, Currency Name or Currency Code\t[Enter 0 to go Back]\n");
+            _Print("-->  ");
+            string choice = MyInputLibrary::ReadLine("");
+            _Print("___________________________________\n\n");
+
+            if(choice == "0")
+                return;
+            
+            _PerformFindCurrency(choice);
+            _PauseScreen();
+        }       
+
+    }
+
+
+};
+
